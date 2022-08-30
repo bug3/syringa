5dbc8f5ca8009c5c9179fe59cb1e4a9d92e7146c
bug3 <bug3dev@gmail.com>
2022-08-30 22:39:35 +0300

    Define create command

--- bin/syringa.js
+++ bin/syringa.js
@@ -1,5 +1,11 @@
 const cli = require('commander');
 
+const create = () => {
+    console.log('create');
+};
+
 cli.name('syringa').description('The Live Injector').version('0.0.1');
+
+cli.command('create').action(create).description('create new project');
 
 cli.parse();
