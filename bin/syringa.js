cd32c4a0956b01f66fc299442e8ad703382be381
bug3 <bug3dev@gmail.com>
2022-08-30 22:40:19 +0300

    Define run command

--- bin/syringa.js
+++ bin/syringa.js
@@ -4,8 +4,13 @@ const create = () => {
     console.log('create');
 };
 
+const run = () => {
+    console.log('run');
+};
+
 cli.name('syringa').description('The Live Injector').version('0.0.1');
 
 cli.command('create').action(create).description('create new project');
+cli.command('run').action(run).description('run the project');
 
 cli.parse();
