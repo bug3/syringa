bbf1b84385977646f219551f4559932843f292d7
bug3 <bug3dev@gmail.com>
2022-09-02 01:10:19 +0300

    Create WebSocket server

--- bin/syringa.js
+++ bin/syringa.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
 const cli = require('commander');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8128 });
 
 const create = () => {
     console.log('create');
 };
 
 const run = () => {
-    console.log('run');
+    wss.on('connection', function connection(ws) {});
 };
 
 cli.name('syringa').description('The Live Injector').version('0.0.1');
