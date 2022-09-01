c8a2718e1795058e3d52a072f30d21e68e84643a
bug3 <bug3dev@gmail.com>
2022-09-02 01:10:55 +0300

    Watch process cwd

--- bin/syringa.js
+++ bin/syringa.js
@@ -2,6 +2,7 @@
 
 const cli = require('commander');
 const WebSocket = require('ws');
+const watch = require('node-watch');
 
 const wss = new WebSocket.Server({ port: 8128 });
 
@@ -10,7 +11,11 @@ const create = () => {
 };
 
 const run = () => {
-    wss.on('connection', function connection(ws) {});
+    wss.on('connection', function connection(ws) {
+        watch(process.cwd(), { recursive: true }, function () {
+            ws.send('fidelio');
+        });
+    });
 };
 
 cli.name('syringa').description('The Live Injector').version('0.0.1');
