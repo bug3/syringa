f6232269594772d7f29d244a58f0446b0e04ea75
bug3 <bug3dev@gmail.com>
2022-09-02 02:24:32 +0300

    Receive WebSocket message

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,7 @@
-console.log('syringa content running');
+const socket = new WebSocket('ws://localhost:8128');
+
+socket.onmessage = function (event) {
+    if (event.data === 'fidelio') {
+        alert('Message received');
+    }
+};
