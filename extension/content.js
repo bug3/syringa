998d808b6a1626de1f5c223d4702b8342c5480cd
bug3 <bug3dev@gmail.com>
2022-09-04 00:26:39 +0300

    Define socket.open func

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,11 @@
 const socket = new WebSocket('ws://localhost:8128');
 
+socket.onopen = function () {
+    createScript('lib/jquery-3.6.1.min.js', false);
+
+    inject();
+};
+
 socket.onmessage = function (event) {
     if (event.data === 'fidelio') {
         alert('Message received');
